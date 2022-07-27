298e29d4bd4c5c0aa6a79b158a6c06ba758431a8
denizayaas <denizayaas@gmail.com>
2022-07-27 18:53:40 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { moneyFormat } from "../helpers";
+
+function Header({ total, money }) {
+  return (
+    <>
+      {total > 0 && money - total !== 0 && (
+        <div className="header">
+          Harcayacak <span>$ {moneyFormat(money - total)}</span> paranız kaldı!
+        </div>
+      )}
+      {total === 0 && (
+        <div className="header">
+          Bakiyeniz: <span>$ {moneyFormat(money)}</span>
+        </div>
+      )}
+      {money - total === 0 && (
+        <div className="header empty">
+          Paran bitti, parasız insan boş insandır!
+        </div>
+      )}
+      <style jsx>{`
+        .header {
+          position: sticky;
+          top: 0;
+          background-color: #0000FF;
+          height: 60px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: #fff;
+          font-size: 24px;
+          letter-spacing: 1px;
+        }
+
+        .header.empty {
+          background: linear-gradient(to bottom, #b82020, #be1414);
+        }
+
+        .header span {
+          margin: 0 10px;
+          font-weight: bold;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default Header;
